0308f4a342f1073d4ff435a1dd044c0a4f63a724
Jeroen S <j.soeurt@gmail.com>
2019-11-29 10:50:56 -0500

    Added notification

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ class Controller {
     view;
 
     constructor(v) {
-        this.view=v;
+        this.view = v;
     }
 
 
@@ -19,6 +19,7 @@ class Controller {
             });
 
             console.log("Success: " + response);
+            this.view.showLoggedIn();
         } catch (error) {
             // Error 😨
             if (error.response) {
@@ -30,7 +31,7 @@ class Controller {
                 if (error.response.status == "401") {
                     this.view.showNotLoggedIn();
                 }
- 
+
             } else if (error.request) {
                 /*
                  * The request was made but no response was received, `error.request`
@@ -47,16 +48,65 @@ class Controller {
 
     }
 
+    createNewAccount = async function () {
+        let username = $("#newUsername").val();
+        let password = $("#newPassword").val();
 
+        if (username == "" || password == "") {
+            this.view.showCreateAccountError("Please enter a username and password!");
+        } else {
+            try {
+                const response = await axios({
+                    method: 'POST',
+                    url: 'http://localhost:3000/account/create',
+                    data: {
+                        "name": username,
+                        "pass": password,
+                    }
+                });
+
+                this.view.showNotification(response.data.status);
+                this.view.showNotLoggedIn();
+            } catch (error) {
+                // Error 😨
+                if (error.response) {
+                    /*
+                     * The request was made and the server responded with a
+                     * status code that falls out of the range of 2xx
+                     */
+                    this.view.showCreateAccountError(error.response.data.msg);
+
+                } else if (error.request) {
+                    /*
+                     * The request was made but no response was received, `error.request`
+                     * is an instance of XMLHttpRequest in the browser and an instance
+                     * of http.ClientRequest in Node.js
+                     */
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request and triggered an Error
+                    console.log('Error', error.message);
+                }
+            }
+
+        }
+    }
 
 }
 
 
-
 $(document).ready(function () {
+    let view = new View;
+    let controller = new Controller(view);
 
-    let controller = new Controller(new View());
+    /*Button handler */
+    $(document).on("click", "#newAccountButton", () => { view.showCreateNewAccount(); });
+    $(document).on("click", "#loginButton", () => { controller.doLogin(); });
+    $(document).on("click", "#backToLoginButton", () => { view.showNotLoggedIn(); });
+    $(document).on("click", "#CreateAccountButton", () => { controller.createNewAccount(); });
+
 
     controller.getLoginStatus();
+
 
 });
--- view.js
+++ view.js
@@ -1,17 +1,43 @@
 class View {
     constructor(){
-
+        $("#notification").hide();
     }
 
     showNotLoggedIn(){
-        console.log("Showing not logged in");
         $(".loggedIn").hide();
-        $(".notLoggeIn").show();
+        $(".notLoggedIn").show();
+
+        /*The initial screen when not logged in is the log in screen. Hide new account div.*/
+        $("#createAccountWrapper").hide();
+
+        /*Hide error message div*/
+        $("#loginError").hide();
+        $("#createAccountError").hide();
     }
 
     showLoggedIn(){
         console.log("Showing logged in");
         $(".loggedIn").show();
-        $(".notLoggeIn").hide();
+        $(".notLoggedIn").hide();
+    }
+
+    showCreateNewAccount(){
+        $("#createAccountWrapper").show();
+        $("#loginWrapper").hide();
+    }
+
+    showCreateAccountError(msg){
+        $("#createAccountError").show("slow");
+        $("#createAccountError").html("<p>"+msg+"</p>");
+    }
+
+    showLoginError(msg){
+        $("#loginError").show();
+        $("#loginError").html("<p>"+msg+"</p>");
+    }
+
+    showNotification(msg){
+        $("#notification").html("<p>"+msg+"</p>");
+        $("#notification").show(setTimeout(() => {  $("#notification").hide("slow"); }, 2000));
     }
 }
